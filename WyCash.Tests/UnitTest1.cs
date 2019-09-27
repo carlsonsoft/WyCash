ca4f197f527ffd2ded473b0c696cf4f1e90d5867
carlson <cui18325787035@gmail.com>
2019-09-27 19:16:00 +0800

    增加说明

--- WyCash.Tests/UnitTest1.cs
+++ WyCash.Tests/UnitTest1.cs
@@ -26,7 +26,16 @@ namespace WyCash.Tests
             Assert.Equal(new Franc(10), five.Times(2));
             Assert.Equal(new Franc(15), five.Times(3));
         }
-
+        //TODO:法郎兑美元2：1
+        // 5美元* 2=10美元
+        // 将amount定义为私有
+        // doller有副作用吗(l连续相乘的时候amount会变化)
+        // 钱必须为整数吗
+        // 实现Equals函数
+        // 实现GethashCode方法
+        // 与空对象判等
+        // 与非同类对象判等
+        // 5瑞士法郎 * 2 = 10 瑞士法郎
 
     }
 }
